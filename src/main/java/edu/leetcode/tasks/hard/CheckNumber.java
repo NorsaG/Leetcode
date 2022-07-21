ed1f669597ee0ecbe7768342461b70cafdc12c88
LazarevEvgeny <norsag@mail.ru>
2022-07-21 16:49:49 +0300

    little fix

--- src/main/java/edu/leetcode/tasks/hard/CheckNumber.java
+++ src/main/java/edu/leetcode/tasks/hard/CheckNumber.java
@@ -1,11 +1,7 @@
 package edu.leetcode.tasks.hard;
 
-import org.junit.Test;
 
 import java.util.regex.Pattern;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 public class CheckNumber {
     private static final Pattern INT_NUMBER_PATTERN = Pattern.compile("(\\d+)");
@@ -54,32 +50,4 @@ public class CheckNumber {
         return (s.contains("e") || s.contains("E"));
     }
 
-    @Test
-    public void testValid() {
-        assertTrue(isNumber("2"));
-        assertTrue(isNumber("0089"));
-        assertTrue(isNumber("-0.1"));
-        assertTrue(isNumber("+3.14"));
-        assertTrue(isNumber("4."));
-        assertTrue(isNumber("-.9"));
-        assertTrue(isNumber("2e10"));
-        assertTrue(isNumber("-90E3"));
-        assertTrue(isNumber("3e+7"));
-        assertTrue(isNumber("+6e-1"));
-        assertTrue(isNumber("53.5e93"));
-        assertTrue(isNumber("-123.456e789"));
-    }
-
-    @Test
-    public void testInvalid() {
-        assertFalse(isNumber("."));
-        assertFalse(isNumber("abc"));
-        assertFalse(isNumber("1a"));
-        assertFalse(isNumber("1e"));
-        assertFalse(isNumber("e3"));
-        assertFalse(isNumber("99e2.5"));
-        assertFalse(isNumber("--6"));
-        assertFalse(isNumber("-+3"));
-        assertFalse(isNumber("95a54e53"));
-    }
 }
